40a319a93e379f82760180ba7bef2d6803f78b9a
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-21 04:58:49 +0100

    Fix rubocop error

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,31 +3,30 @@ import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchMessages = () => async (dispatch) => {
-    await axios.get('http://localhost:3000/api/v1/messages',{
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-    })
-    .then(response => {
-        console.log(response);
-        dispatch(fetchMessages(response.data));
-    })
-    .catch(error => {
-        console.log(error);
+  await axios.get('http://localhost:3000/api/v1/messages', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+  })
+    .then((response) => {
+      dispatch({
+        type: 'FETCH_MESSAGES',
+        payload: response.data,
+      });
     });
 };
 
 const messagesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'FETCH_MESSAGES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'FETCH_MESSAGES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export const store = configureStore({
-    reducer: messagesReducer,
+  reducer: messagesReducer,
 }, applyMiddleware(thunk));
