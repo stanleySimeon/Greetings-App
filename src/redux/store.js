b55787739743533de1ef17714fa7f6eb7cd81588
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-24 03:24:32 +0100

    Api url

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import thunk from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
 import { applyMiddleware } from 'redux';
 import axios from 'axios';
-const API = 'https://greetngs.herokuapp.com/api/v1/messages';
+const API = 'https://greetngs.herokuapp.com/greetings';
 
 export const GET_MESSAGES = 'FETCH_MESSAGES';
 
